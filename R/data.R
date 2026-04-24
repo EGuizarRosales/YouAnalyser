1a206f15d68f7778bffb2e03213514c8b141c52f
Emmanuel Guizar Rosales YouGov <emmanuel.guizarrosales@gmail.com>
2026-04-24 19:24:48 +0200

    Add YouGov colors dataset and expand kda_regression() vignette docs

--- R/data.R
+++ R/data.R
@@ -114,3 +114,12 @@
 #'   ...
 #' }
 "bkw_labels"
+
+#' YouGov Colors
+#'
+#' A named character vector containing the official YouGov colors with their corresponding hex codes.
+#' Colors were sourced from the YouGov Marketing Portal
+#'
+#' @format ## `yougov_colors`
+#' A named character vector with 24 elements. Access each element using e.g. `yougov_colors[["Red 1"]]` to get the hex code for "Red 1".
+"yougov_colors"
---
+++ data-raw/yougov_colors.R
@@ -0,0 +1,29 @@
+# fmt: skip
+yougov_colors <- c(
+    "Red 1"      = "#ff412c",
+    "Purple 1"   = "#9f29ff",
+    "Teal 1"     = "#31caa8",
+    "Pink 1"     = "#ff2380",
+    "Blue 1"     = "#06a6ee",
+    "Yellow 1"   = "#ffba22",
+    "Purple 2"   = "#6600ab",
+    "Pink 2"     = "#ff7bb3",
+    "Navy 1"     = "#003cab",
+    "Teal 2"     = "#83dfcb",
+    "Red 2"      = "#c20800",
+    "Lavender 1" = "#c57fff",
+    "Salmon 1"   = "#ff8d80",
+    "Blue 2"     = "#6acaf5",
+    "Green 1"    = "#006b46",
+    "Salmon 2"   = "#ff6756",
+    "Purple 3"   = "#8001d8",
+    "Pink 3"     = "#ff4f99",
+    "Blue 3"     = "#006bdb",
+    "Teal 3"     = "#5ad5b9",
+    "Red 3"      = "#e8281e",
+    "Lavender 2" = "#b254ff",
+    "Green 2"    = "#149678",
+    "Yellow 2"   = "#ffe146"
+  )
+
+usethis::use_data(yougov_colors, overwrite = TRUE)
